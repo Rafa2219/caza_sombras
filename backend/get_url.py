78db58c74acee264056abeac1c703dcf923a99d6
Rafa2219 <161893100+Rafa2219@users.noreply.github.com>
2025-10-31 12:00:54 -0600

    update get_url.py

--- backend/get_url.py
+++ backend/get_url.py
@@ -14,15 +14,15 @@ url_pattern = re.compile(r"https://[a-zA-Z0-9\-]+\.serveo\.net")
 output_file = "public_url.txt"
 
 with open(output_file, "w") as f:
-      for line in process.stdout:
-                print(line.strip())  # Muestra la salida en consola
+    for line in process.stdout:
+        print(line.strip())  # Muestra la salida en consola
         match = url_pattern.search(line)
         if match:
-                      url = match.group(0)
-                      f.write(url + "\n")
-                      f.flush()
-                      print(f"✅ URL pública guardada en {output_file}: {url}")
-                      break
+            url = match.group(0)
+            f.write(url + "\n")
+            f.flush()
+            print(f"✅ URL pública guardada en {output_file}: {url}")
+            break
 
 print("⏳ Esperando mantener el túnel activo...")
 process.wait()
